d3c9c90104b72b268f20460374f68f371fc9f20d
Kevin Rion <44882353+kevinrion@users.noreply.github.com>
2026-06-19 12:41:24 +0100

    Add Tailwind CSS integration, create configuration files, and implement button component; update package dependencies and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next'
 import Breadcrumb from '@/components/Breadcrumb'
 import '@/lib/fontawesome'
 import './globals.css'
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: 'Kanban Board',
@@ -14,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <Breadcrumb />
         {children}
